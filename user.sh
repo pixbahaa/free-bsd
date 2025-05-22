ca4bf4e9005d68ebdafb51b355bb3f1fc9df8ad9
pixbahaa <pixelatedbahaa@gmail.com>
2025-05-22 18:08:03 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,6 +1,6 @@
 doas pkg update
 doas pkg upgrade -y
-doas pkg install -y drm-kmod xorg alacritty wayland seatd wayfire wf-shell swaylock-effects swayidle wlogout kanshi mako wlsunset
+doas pkg install -y drm-kmod xorg alacritty wayland seatd wayfire wf-shell swaylock-effects swayidle wlogout kanshi slurp grim wofi wlsunset
 doas sysrc kld_list+=i915kms
 doas sysrc seatd_enable="YES"
 doas sysrc dbus_enable="YES"
