a3ff747a439011cf9e1974fdd6f7527f8dcfdc78
pixbahaa <pixelatedbahaa@gmail.com>
2025-05-22 17:02:52 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 doas pkg update
 doas pkg upgrade -y
-doas pkg install -y drm-kmod xorg
+doas pkg install -y drm-kmod xorg alacritty wayland seatd wayfire wf-shell swaylock-effects swayidle wlogout kanshi mako wlsunset
+doas sysrc kld_list+=i915kms
+doas sysrc seatd_enable="YES"
+mkdir ~/.config/wayfire
+cp /usr/local/share/examples/wayfire/wayfire.ini ~/.config/wayfire
