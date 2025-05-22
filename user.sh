68261a6d47c632c2f51ed26da449487812fef4fe
pixbahaa <pixelatedbahaa@gmail.com>
2025-05-22 17:32:55 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -3,5 +3,6 @@ doas pkg upgrade -y
 doas pkg install -y drm-kmod xorg alacritty wayland seatd wayfire wf-shell swaylock-effects swayidle wlogout kanshi mako wlsunset
 doas sysrc kld_list+=i915kms
 doas sysrc seatd_enable="YES"
+doas sysrc dbus_enable="YES"
 mkdir ~/.config/wayfire
 cp /usr/local/share/examples/wayfire/wayfire.ini ~/.config/wayfire
