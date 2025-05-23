c48b2439560e80fd3103c5237f414b5224a4766a
pixbahaa <pixelatedbahaa@gmail.com>
2025-05-23 10:59:42 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,6 +1,6 @@
 doas pkg update
 doas pkg upgrade -y
-doas pkg install -y drm-kmod xorg alacritty wayland seatd wayfire wf-shell swaylock-effects swayidle wlogout kanshi slurp grim wofi wlsunset
+doas pkg install -y drm-kmod xorg alacritty wayland seatd wayfire wf-shell swaylock-effects swayidle wlogout kanshi slurp grim wofi wlsunset noto-sans noto-sans-mono noto-sans-arabic noto-emoji vim fastfetch
 doas sysrc kld_list+=i915kms
 doas sysrc seatd_enable="YES"
 doas sysrc dbus_enable="YES"
