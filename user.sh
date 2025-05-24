84d3f4078b05ff8893c184baf76671184f8aee8b
bahaa <pixelatedbahaa@gmail.com>
2025-05-24 14:02:32 +0100

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,10 +1,11 @@
 #/bin/sh
 
+cd
 doas pkg update
 doas pkg upgrade -y
 doas pkg install -y drm-kmod xorg alacritty wayland seatd wayfire wf-shell swaylock-effects swayidle wlogout kanshi slurp grim wofi wlsunset noto-sans noto-sans-mono noto-sans-arabic noto-emoji vim fastfetch
 doas sysrc kld_list+=i915kms
 doas sysrc seatd_enable="YES"
 doas sysrc dbus_enable="YES"
-mkdir -p /home/bahaa/.config/wayfire
-cp /home/bahaa/freebsd/wayfire.ini /home/bahaa/.config/wayfire/
+mkdir -p .config/wayfire
+cp freebsd/wayfire.ini /home/bahaa/.config/wayfire/
